93b42b9f4aef235764201eb80f44d5072d78979f
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-06-01 00:08:57 +0300

    Всё доделал. Осталось навести порядок.

--- src/entity/classes/salary.py
+++ src/entity/classes/salary.py
@@ -3,39 +3,28 @@ from _pydecimal import Decimal
 from dataclasses import dataclass
 
 
-def fabric_salary_hh(json):
-    if json is not None:
-        return Salary(json.get("currency"), fabric_decimal(json.get("from")), fabric_decimal(json.get("to")))
+def fabric_salary_hh(json_hh):
+    if json_hh is not None:
+        return Salary(json_hh.get("currency"), fabric_float_json(json_hh.get("from")), fabric_float_json(json_hh.get("to")))
     else:
         return None
 
 
 def fabric_salary_sj(currency, s_from, s_to):
-    return Salary(currency.upper(), Decimal(s_from), Decimal(s_to))
+    return Salary(currency.upper(), fabric_float_json(s_from), fabric_float_json(s_to))
 
 
-def fabric_decimal(json_object):
+def fabric_float_json(json_object):
     if json_object is not None:
-        return Decimal(json_object)
+        return float(json_object)
     return None
-
-
-class SalaryEncoder(json.JSONEncoder):
-    def default(self, obj):
-        print("tl23ktgio23jgoiijog")
-        if isinstance(obj, Salary):
-            return obj.__dict__
-        return json.JSONEncoder.default(self, obj)
 
 
 @dataclass
 class Salary:
     currency: str
-    s_from: Decimal
-    s_to: Decimal
-
-    def print_sal(self):
-        print(f'ЗП: от {self.s_from}, до {self.s_to}')
+    s_from: float
+    s_to: float
 
     def get_s_from(self):
         return self.s_from
@@ -46,10 +35,13 @@ class Salary:
     def get_currency(self):
         return self.currency
 
-    def compare_total_mag_to(self, other):
-        if other.s_to is None or other.s_to.compare(Decimal('0')) == 0:
+    def str(self):
+        return f'в {self.currency}: от {self.s_from}, до {self.s_to}.'
+
+    def __lt__(self, other):
+        if other.s_to is None or other.s_to == 0:
             return True
-        if self.s_to is None or self.s_to.compare(Decimal('0')) == 0:
+        if self.s_to is None or self.s_to == 0:
             return False
 
-        return self.s_to.compare(other.s_to) == 1
+        return self.s_to > other.s_to
